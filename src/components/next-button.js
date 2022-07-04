7c1a086fd659d323fc5a833a3fb7bafe5e5e068b
Hoyoung Jung <honux77@gmail.com>
2022-07-04 16:03:18 +0900

    다음곡 자동재생 구현

--- src/components/next-button.js
+++ src/components/next-button.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-const NextButton = ({first, audios, isPlaying, current, setCurrent})=> {    
+const NextButton = ({first, audios, isPlaying, current, setCurrent})=> {
 
     if(first) return;
 
     const handlePlayButton = (e)=> {            
-        
-        console.log("stop audio")
         audios[current].pause();
         audios[current].currentTime = 0;
 
