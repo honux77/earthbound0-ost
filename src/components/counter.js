48577337f5b9d014a356e24e4f3e8052f4f29ee9
Hoyoung Jung <honux77@gmail.com>
2026-01-23 01:17:33 +0900

    배포를 위해 경고 수정

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,7 +4,7 @@ const Counter = ({first}) => {
     return (
         <div className="Counter">
             <a href="https://hits.seeyoufarm.com">
-                <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhonux77.github.io%2Fearthbound0-ost%2F&count_bg=%23FF0000&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
+                <img alt="site hits" src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhonux77.github.io%2Fearthbound0-ost%2F&count_bg=%23FF0000&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
             </a>
         </div>
     );
